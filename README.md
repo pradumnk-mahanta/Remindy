ea14847dd1ae9b9fbc1954779f41b237e18e16e9
Pradumn Kumar Mahanta <pradumnk.mahanta@gmail.com>
2017-01-18 15:30:07 +0530

    Added Twilio
    - Can Send Instant Message Now.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # What is this?
 This is a simple implementation of <b>Twilio's</b> SMS API with Node.JS.
 # How can I test this?
-Head over to <a herf="re-mindy.heroku.com">the deployed application</a>, to test it and explore its functionalities.
+Head over to <a herf="https://re-mindy.herokuapp.com/">the deployed application</a>, to test it and explore its functionalities.
+# Preview
+This is a preview of the web-application that I created for testing purposes.
+![Web Application](assets/web-application.png)
+This is the kind of message you would recieve on your registered mobile.
+![SMS](assets/message.png)
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoDB = require('mongodb');
+var monk = require('monk');
+var db = monk('// DB URI');
+var accountSid = 'TWILIO ACCOUNT ID';
+var authToken = 'AUTHENTICATION TOKEN';
+var fromNumber = 'TWILIO NUMBER';
+var client = require('twilio')(accountSid, authToken);
+
+
+//Send Function for Twilio (Simplified)
+var sendSMS = function(to, message) {
+  client.messages.create({
+    body: message,
+    to: to,
+    from: fromNumber
+  }, function(err, data) {
+        //Handle Error Here;
+  });
+};
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -12,6 +38,24 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.post('/remindme', function(request, response) {
+  console.log('+' + request.body.countryCode + request.body.phoneNumber, request.body.remindText);
+
+  //Writing to mongoDB Simplified.
+  var collection = db.get('reminders');
+  collection.insert(request.body, function(error, doc){
+    if(error){
+      console.log("Could not add to database.");
+    }else{
+      console.log("Data processed successfully.");
+    }
+  });
+
+  //Calling the Send Function
+  sendSMS('+' + request.body.countryCode + request.body.phoneNumber, request.body.remindText);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node Application is Running on PORT:', app.get('port'));
